679d7e77ec4d4a103d0672d9d6afeaf41000a97a
thisgun <thisgun@naver.com>
2017-09-10 20:54:01 +0900

    import

---
+++ adm/shop_admin/item_export.php
@@ -0,0 +1,106 @@
+<?php
+$sub_menu = '500720';
+include_once('./_common.php');
+include_once(G5_LIB_PATH.'/xml.lib.php');
+
+check_demo();
+
+auth_check($auth[$sub_menu], "r");
+
+function yc5_get_item_image_url($it_id){
+    return G5_DATA_URL.'/item/';
+}
+
+if( isset($_POST['action']) && 'export_data' === $_POST['action'] ){
+
+    $items = array();
+
+    $sql = "select * from `{$g5['g5_shop_category_table']}` ";
+    
+    $result = sql_query($sql);
+
+    for($i=0; $cate=sql_fetch_array($result); $i++) {
+        
+        foreach($cate as $k=>$v){
+            $items['categories']['cate'.$i][$k] = $v;
+        }
+    }
+
+    $sql = " select * from `{$g5['g5_shop_item_table']}` ";
+
+    $item_result = sql_query($sql);
+
+    for($i=0; $item=sql_fetch_array($item_result); $i++) {
+        $it_id = $item['it_id'];
+        
+        $items['items']['item'.$i]['@attributes'] = array('it_id' => $it_id);
+        foreach($item as $k=>$v){
+
+            if( preg_match('/^it_img/i', $k) ){
+                $v = $v ? yc5_get_item_image_url($it_id).$v : '';
+            }
+
+            $items['items']['item'.$i][$k] = $v;
+        }
+
+        $sql = "select * from `{$g5['g5_shop_item_option_table']}` where it_id = ".$it_id." order by io_no asc ";
+        $op_result = sql_query($sql);
+        
+        for($j=0; $option=sql_fetch_array($op_result); $j++) {
+            
+            //반드시 chr30문자를 ,로 replace 해야 한다. chr(30) 문자를 xml 문서에서 표시하면 오류가 나기 때문에
+            $option['io_id'] = str_replace(chr(30), ',', $option['io_id']);
+
+            $items['items']['item'.$i]['options']['option'.$j] = $option;
+        }
+    }
+
+    $filename = 'youngcart_data_'.G5_TIME_YMD.'.xml';
+
+    /* Print header */
+    header( 'Content-Description: File Transfer' );
+    header( 'Content-Disposition: attachment; filename=' . $filename );
+    header( 'Content-Type: text/xml; charset=UTF-8', true );
+
+    $xml = new Array2xml();
+    $xml->setFilterNumbersInTags(array('item'));
+    echo $xml->convert($items);
+
+    exit;
+
+}
+
+$g5['title'] = '상품 내보내기';
+include_once (G5_ADMIN_PATH.'/admin.head.php');
+?>
+
+<div class="local_sch02 local_sch">
+
+    <div>
+
+        <form id="export-item-form" method="post">
+
+        <p>
+        <label>상품, 사용후기, 이미지, 카테고리와 태그 등을 xml 파일로 내보냅니다.</label>
+        <input type="hidden" name="action" value="export_data">
+        </p>
+
+        <input type="submit" name="submit" id="submit" class="button" value="상품 내보내기">
+
+        </form>
+        
+    </div>
+
+</div>
+
+<script>
+
+jQuery(function($){
+
+});
+
+</script>
+
+<?php
+include_once (G5_ADMIN_PATH.'/admin.tail.php');
+?>
